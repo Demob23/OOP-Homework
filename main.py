52ed640fca5e97eb8934bb5841336ea2dc523a54
Dmitriy Semenov <working.demob@gmail.com>
2023-01-17 12:34:45 +0400

    Task #2 completed

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ class Student():
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return "Ошибка"
+
 
 class Mentor():
     def __init__(self, name, surname):
@@ -15,7 +24,27 @@ class Mentor():
         self.courses_attached = []
 
 
-cool_mentor = Mentor("Some", "Buddy")
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return "Ошибка"
+
+
+cool_mentor = Lecturer("Some", "Buddy")
 cool_mentor.courses_attached += ["Python"]
 print(cool_mentor.courses_attached)
 cool_mentor.courses_attached += ["Golang"]
@@ -27,3 +56,10 @@ best_student.courses_in_progress += ["Python"]
 best_student.grades["C#"] = [10, 8, 9, 10, 10]
 best_student.grades["Python"] = [10, 10]
 print(best_student.grades)
+
+reviewer = Reviewer("Name", "Surname")
+reviewer.rate_hw(best_student, "Python", 9)
+print(best_student.grades)
+
+best_student.rate_lecture(cool_mentor, "Python", 8)
+print(cool_mentor.grades)
