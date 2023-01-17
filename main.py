e53d158a2c1d285217d0d61e85dc8c84f4ecf483
Dmitriy Semenov <working.demob@gmail.com>
2023-01-17 14:34:02 +0400

    Task #3 and #4 completed

--- main.py
+++ main.py
@@ -1,4 +1,80 @@
+def get_average_grades(grades):
+    average_grade = 0
+    num_of_sums = 0
+    for key in grades:
+        for grade in grades[key]:
+            average_grade += grade
+            num_of_sums += 1
+    return average_grade / num_of_sums
+
+
+def calculate_students_average_grades(students: list, course):
+    average_grade = 0
+    num_of_sums = 0
+    for student in students:
+        if course in student.courses_in_progress:
+            for grade in student.grades[course]:
+                average_grade += grade
+                num_of_sums += 1
+    average_grades = average_grade/num_of_sums
+    return average_grades
+
+
+def calculate_lectors_average_grades(lectors: list, course):
+    average_grade = 0
+    num_of_sums = 0
+    for lector in lectors:
+        if course in lector.courses_attached:
+            for grade in lector.grades[course]:
+                average_grade += grade
+                num_of_sums += 1
+    average_grades = average_grade/num_of_sums
+    return average_grades
+
+
 class Student():
+    def __eq__(self, other):
+        if isinstance(other, Student):
+            return get_average_grades(self.grades) == get_average_grades(other.grades)
+        else:
+            return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, Student):
+            return get_average_grades(self.grades) != get_average_grades(other.grades)
+        else:
+            return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, Student):
+            return get_average_grades(self.grades) < get_average_grades(other.grades)
+        else:
+            return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, Student):
+            return get_average_grades(self.grades) <= get_average_grades(other.grades)
+        else:
+            return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, Student):
+            return get_average_grades(self.grades) > get_average_grades(other.grades)
+        else:
+            return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, Student):
+            return get_average_grades(self.grades) >= get_average_grades(other.grades)
+        else:
+            return NotImplemented
+    def __str__(self):
+        return f"Имя: {self.name}\n" \
+               f"Фамилия: {self.surname}\n" \
+               f"Средняя оценка за домашние задания: {get_average_grades(self.grades)}\n" \
+               f"Курсы в процессе изучения: {''.join(self.courses_in_progress)}\n" \
+               f"Завершённые курсы: {''.join(self.finished_courses)}"
+
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
@@ -25,12 +101,55 @@ class Mentor():
 
 
 class Lecturer(Mentor):
+    def __eq__(self, other):
+        if isinstance(other, Lecturer):
+            return get_average_grades(self.grades) == get_average_grades(other.grades)
+        else:
+            return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, Lecturer):
+            return get_average_grades(self.grades) != get_average_grades(other.grades)
+        else:
+            return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, Lecturer):
+            return get_average_grades(self.grades) < get_average_grades(other.grades)
+        else:
+            return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, Lecturer):
+            return get_average_grades(self.grades) <= get_average_grades(other.grades)
+        else:
+            return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, Lecturer):
+            return get_average_grades(self.grades) > get_average_grades(other.grades)
+        else:
+            return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, Lecturer):
+            return get_average_grades(self.grades) >= get_average_grades(other.grades)
+        else:
+            return NotImplemented
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
 
 
+    def __str__(self):
+        return f"Имя: {self.name}\n" \
+               f"Фамилия: {self.surname}\n" \
+               f"Средняя оценка за лекции: {get_average_grades(self.grades)}"
+
 class Reviewer(Mentor):
+    def __str__(self):
+        return f"Имя: {self.name}\n" \
+               f"Фамилия: {self.surname}"
     def __init__(self, name, surname):
         super().__init__(name, surname)
 
@@ -44,22 +163,65 @@ class Reviewer(Mentor):
             return "Ошибка"
 
 
-cool_mentor = Lecturer("Some", "Buddy")
-cool_mentor.courses_attached += ["Python"]
-print(cool_mentor.courses_attached)
-cool_mentor.courses_attached += ["Golang"]
-print(cool_mentor.courses_attached)
 
-best_student = Student("Big", "Floppa", "Russian Cat")
-best_student.finished_courses += ["C#"]
-best_student.courses_in_progress += ["Python"]
-best_student.grades["C#"] = [10, 8, 9, 10, 10]
-best_student.grades["Python"] = [10, 10]
-print(best_student.grades)
+student_1 = Student("Василий", "Каруселин", "M")
+student_2 = Student("Александра", "Иванова", "F")
+lecturer_1 = Lecturer("Сергей", "Куприянов")
+lecturer_2 = Lecturer("Павел", "Андреев")
+reviewer_1 = Reviewer("Иван", "Обломов")
+reviewer_2 = Reviewer("Галина", "Терентьева")
 
-reviewer = Reviewer("Name", "Surname")
-reviewer.rate_hw(best_student, "Python", 9)
-print(best_student.grades)
+student_1.finished_courses += ["C#"]
+student_1.courses_in_progress += ["Python", "Go"]
 
-best_student.rate_lecture(cool_mentor, "Python", 8)
-print(cool_mentor.grades)
+student_2.finished_courses += ["Java"]
+student_2.courses_in_progress += ["Python"]
+
+lecturer_1.courses_attached += ["Python"]
+lecturer_2.courses_attached += ["Go", "Python"]
+
+reviewer_1.rate_hw(student_1, "Python", 10)
+reviewer_1.rate_hw(student_1, "Python", 9)
+reviewer_1.rate_hw(student_1, "Go", 9)
+reviewer_1.rate_hw(student_1, "Python", 10)
+reviewer_1.rate_hw(student_1, "Python", 10)
+reviewer_1.rate_hw(student_1, "Python", 10)
+reviewer_1.rate_hw(student_1, "Go", 10)
+
+reviewer_1.rate_hw(student_2, "Python", 8)
+reviewer_1.rate_hw(student_2, "Python", 9)
+reviewer_1.rate_hw(student_2, "Python", 10)
+
+reviewer_2.rate_hw(student_1, "Python", 9)
+reviewer_2.rate_hw(student_1, "Python", 9)
+reviewer_2.rate_hw(student_1, "Python", 9)
+reviewer_2.rate_hw(student_1, "Python", 10)
+reviewer_2.rate_hw(student_1, "Python", 8)
+reviewer_2.rate_hw(student_1, "Python", 8)
+reviewer_2.rate_hw(student_1, "Python", 10)
+
+reviewer_2.rate_hw(student_2, "Python", 8)
+reviewer_2.rate_hw(student_2, "Python", 9)
+reviewer_2.rate_hw(student_2, "Python", 10)
+
+student_1.rate_lecture(lecturer_2, "Go", 8)
+student_1.rate_lecture(lecturer_1, "Python", 10)
+student_1.rate_lecture(lecturer_2, "Go", 10)
+
+student_2.rate_lecture(lecturer_1, "Python", 8)
+student_2.rate_lecture(lecturer_1, "Python", 10)
+student_2.rate_lecture(lecturer_1, "Python", 10)
+
+print(reviewer_1)
+print(reviewer_2)
+print(lecturer_1)
+print(lecturer_2)
+print(student_1)
+print(student_2)
+print(student_1 > student_2)
+print(student_1 < student_2)
+print(student_2 > student_1)
+print(lecturer_1 == lecturer_2)
+print(lecturer_1 != lecturer_2)
+print(calculate_students_average_grades([student_1, student_2], "Go"))
+print(calculate_lectors_average_grades([lecturer_1, lecturer_2], "Go"))
